a6e4d8c7455e8d919eba0515c39c4960850c6e3f
ianlienfa <ianlienfa@gmail.com>
2021-02-02 11:42:25 +0800

    First Commit

---
+++ app/src/main/java/com/example/artfestproject1/Test.java
@@ -0,0 +1,27 @@
+package com.example.artfestproject1;
+
+import com.example.artfestproject1.MyImage.*;
+
+import org.bytedeco.opencv.opencv_core.Mat;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ImageGallery.DIRPATH ="/Users/linenyan/Coding/ArtFestProject0/app/src/main/java/com/example/artfestproject0/MyImage/";
+//        ImageGallery.DIRPATH = "/Users/linyanting/Desktop/ArtFestProject0/app/src/main/java/com/example/artfestproject0/MyImage/output.jpg";
+//        ImageGallery.DIRPATH="/Users/Administrator/AndroidStudioProjects/ArtFestProject0/app/src/main/java/com/example/artfestproject0/MyImage/output.jpg";
+
+        Mat image = ImageGallery.stdLoadImg("rex.jpg");
+        ImageGallery imageGallery = new ImageGallery(image, 108, 108);
+
+//        Mat img_user = ImageGallery.stdLoadImg("1.jpg");
+//        Mat img_base = ImageGallery.stdLoadImg("[9][1].jpg");
+//        img_new = imageGallery.algorithm_Tim(img_user, img_new);
+//        img_new = imageGallery.algorithm_shiuan(img_user, img_new);
+        Mat img_user = ImageGallery.stdLoadImg("[4][7].jpg");
+        Mat img_base = ImageGallery.stdLoadImg("[1][7].jpg");
+        Mat img_new = imageGallery.algorithm_BAI(img_user, img_base);
+        ImageGallery.stdSaveImg(img_new, "output.jpg");
+
+    }
+}
