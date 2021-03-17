5314ab0bd10042fd028a9b6845cd73db3f71806b
hoho-nctu <dh990922@gmail.com>
2021-03-17 09:19:58 +0800

    ui

---
+++ app/src/main/java/com/example/artfestproject1/ExplainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.artfestproject1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExplainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
