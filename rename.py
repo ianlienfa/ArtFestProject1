b2015f6364589c870062934a288a7b0799f2c08d
kanido386 <kanido386@gmail.com>
2021-03-26 23:03:25 +0800

    Edit width and height

--- rename.py
+++ rename.py
@@ -3,7 +3,7 @@ from shutil import copyfile
 
 os.mkdir('./generated')
 
-for j in range(10):
-  for i in range(10):
-    copyfile(f'[{i}][{j}].jpg', f'generated/small_image{j*10+i}.jpg')
+for j in range(5):
+  for i in range(5):
+    copyfile(f'[{i}][{j}].jpg', f'generated/small_image{j*5+i}.jpg')
     # os.rename(f'[{i}][{j}].jpg', f'smallImage{i*10+j}.jpg')
