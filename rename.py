b6cc12d6f7772ff671a1bb96f8e3489f76c3c967
kanido386 <kanido386@gmail.com>
2021-05-05 20:12:27 +0800

    Fix small part

--- rename.py
+++ rename.py
@@ -5,5 +5,5 @@ os.mkdir('./generated')
 
 for j in range(12):
   for i in range(33):
-    copyfile(f'[{i}][{j}].jpg', f'generated/small_image{j*5+i}.jpg')
+    copyfile(f'[{i}][{j}].jpg', f'generated/small_image{j*33+i}.jpg')
     # os.rename(f'[{i}][{j}].jpg', f'smallImage{i*10+j}.jpg')
